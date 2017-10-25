7113ffdf291b216d3d344f2ddf1e28de679f6648
INJAZ LLC <31472988+injazllc@users.noreply.github.com>
2017-10-25 13:45:33 +0400

    Add files via upload

---
+++ website_navigation_language_menu/__manifest__.py
@@ -0,0 +1,17 @@
+{
+	'name': 'Website Navigation Language Menu',
+	'summary': """
+		Enabling Language Menu On Header
+	""",
+	'description': """
+	""",
+	'website': 'https://www.injaz.ae',
+	'version': '1.0',
+	'author': 'INJAZ LLC',
+	'data': [
+		'views/layout.xml'
+	],
+	'category': 'Website',
+	'depends': ['website'],
+	'application': True,
+}
